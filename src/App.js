9d12882cb573746d1124b979baa8042f69fcf0ca
Xeyyam Elizade <xeyyamelizade5@gmail.com>
2025-03-03 01:00:19 +0400

    hover effekt

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function Home() {
     }
   };
 
+
   const scrollToSection = (id) => {
     const section = document.querySelector(id);
     if (section) {
@@ -58,20 +59,21 @@ function Home() {
 
   return (
     <div className="min-h-screen text-white flex flex-col items-center overflow-y-auto bg-black" style={{ backgroundImage: "url('/desktop/bu.webp')", backgroundSize: "cover", backgroundPosition: "center" }}>
-      <header className="w-full bg-gradient-to-r from-gray-900 to-black py-6 fixed top-0 shadow-2xl z-50 flex justify-between items-center px-10">
-        <div className="flex items-center text-2xl text-white">
-          <FaUserCircle className="mr-2 text-green-400" />
-          <span>Portfolio</span>
+      <header className="w-full bg-black py-6 fixed top-0 shadow-2xl z-50 flex justify-between items-center px-10">
+        <div className="flex items-center text-xl text-white">
+          <FaUserCircle className="mr-2 text-green-400 text-3xl" />
+          <span className="font-semibold tracking-widest">Portfolio</span>
         </div>
-        <nav className="flex space-x-10">
+        <nav className="flex space-x-6">
           {[{ id: "#home", title: "Əsas" }, { id: "#projects", title: "Layihələr" }, { id: "#experience", title: "Təcrübə" }, { id: "#skills", title: "Bacarıqlar" }, { id: "#about", title: "Haqqında" }, { id: "#contact", title: "Əlaqə" }].map((item) => (
             <motion.button
               key={item.id}
               onClick={() => scrollToSection(item.id)}
-              className="text-lg text-white hover:text-green-400 transition-all cursor-pointer px-4 py-2 rounded-lg bg-transparent hover:bg-green-700"
-              whileHover={{ scale: 1.3 }}
+              className="text-sm text-white px-3 py-2 relative cursor-pointer overflow-hidden group"
+              whileHover={{ scale: 1.1 }}
             >
               {item.title}
+              <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-orange-500 transition-all duration-500 group-hover:w-full"></span>
             </motion.button>
           ))}
         </nav>
@@ -101,7 +103,7 @@ function Home() {
                 <input type="email" name="email" placeholder="Email" onChange={handleChange} className="p-3 border border-white rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-blue-500" required />
                 <input type="tel" name="phone" placeholder="Nömrə" onChange={handleChange} className="p-3 border border-white rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-blue-500" required />
                 <textarea name="comment" placeholder="Komment" onChange={handleChange} className="col-span-2 p-3 border border-white rounded-lg bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none h-40 overflow-y-auto scrollbar-thin scrollbar-thumb-green-500 scrollbar-track-gray-700 scrollbar-thumb-rounded-full scrollbar-track-rounded-full" required></textarea>
-                <button type="submit" onSubmit={onSubmit} className="col-span-2 p-3 bg-green-500 hover:bg-green-700 rounded-lg shadow-lg transition-all">Göndər</button>
+                <button type="submit" className="col-span-2 p-3 bg-green-500 hover:bg-green-700 rounded-lg shadow-lg transition-all">Göndər</button>
               </form>
               <div className="flex space-x-4 mt-10">
                 <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="text-white text-3xl hover:text-yellow-400 transition-all duration-1500">
